9501046a7aa90af9789f8778177df8299a89a34a
Ahmed El Ouali <ahmel-ou@cbr6s5.42barcelona.com>
2025-02-27 16:16:19 +0100

    100%

--- minitalk.h
+++ minitalk.h
@@ -1,13 +1,13 @@
 #ifndef MINITALK_H
 # define MINITALK_H
-# include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <sys/types.h>
+
 # include <bits/sigaction.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <unistd.h>
 
-int ft_atoi(const char *nptr);
-int ft_strlen(char *str);
-
+int	ft_atoi(const char *nptr);
+int	ft_strlen(char *str);
 
 #endif
